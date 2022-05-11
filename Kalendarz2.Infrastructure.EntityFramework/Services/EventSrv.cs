2e38fb6a82407f118fb1887c690a8731c4cf326d
AlbertK10 <103700277+AlbertK10@users.noreply.github.com>
2022-05-11 18:09:14 +0200

    Add files via upload

---
+++ Kalendarz2.Infrastructure.EntityFramework/Services/EventSrv.cs
@@ -0,0 +1,36 @@
+﻿using Kalendarz2.Domain.Common;
+using Kalendarz2.Domain.Common.Exceptions;
+using Kalendarz2.Domain.Common.Models.User;
+using Kalendarz2.Domain.Interfaces.Infrastucture;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Kalendarz2.Infrastructure.EntityFramework;
+/*
+public class EventSrv : IEventSrv
+{
+
+
+    public EventSrv()
+    {
+
+    }
+    public EventDTO AddEvent(AddEventDTO addeventDTO)
+    {
+
+    }
+    public EventDTO ModifyEvent(ModifyEventDTO modifyeventDTO)
+    {
+
+    }
+
+}
+
+*/
