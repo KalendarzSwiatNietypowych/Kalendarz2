1ff15c376c81d6dac45cc96c2be827857079ce85
AlbertK10 <103700277+AlbertK10@users.noreply.github.com>
2022-05-11 18:05:31 +0200

    Add files via upload

---
+++ Domain.Facades/EventFcd.cs
@@ -0,0 +1,24 @@
+﻿using Kalendarz2.Domain.Common;
+using Kalendarz2.Domain.Interfaces.Facades;
+using Kalendarz2.Domain.Interfaces.Infrastucture;
+
+namespace Kalendarz2.Domain.Facades;
+
+public class EventFcd : IEventFcd
+{
+    private readonly IEventSrv _eventSrv;
+
+    public EventFcd(IEventSrv eventSrv)
+    {
+        _eventSrv = eventSrv;
+    }
+    public EventDTO AddEvent(AddEventDTO addeventDTO)
+    {
+        return _eventSrv.AddEvent(addeventDTO);
+    }
+    public EventDTO ModifyEvent(ModifyEventDTO modifyeventDTO)
+    {
+        return _eventSrv.ModifyEvent(modifyeventDTO);
+    }
+
+}
