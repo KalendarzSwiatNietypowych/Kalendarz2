9df1ad521f765db70406c2d1374c6744d6ecbf56
AlbertK10 <103700277+AlbertK10@users.noreply.github.com>
2022-05-11 18:08:17 +0200

    Add files via upload

---
+++ Kalendarz2.Domain.Interfaces.Infrastucture/IEventSrv.cs
@@ -0,0 +1,10 @@
+﻿using Kalendarz2.Domain.Common;
+
+namespace Kalendarz2.Domain.Interfaces.Infrastucture;
+
+public interface IEventSrv
+{
+    EventDTO AddEvent(AddEventDTO addeventDTO);
+    EventDTO ModifyEvent(ModifyEventDTO modifyeventDTO);
+
+}
