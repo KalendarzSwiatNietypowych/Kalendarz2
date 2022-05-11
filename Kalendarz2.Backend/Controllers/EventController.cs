b07a99798be920ea8e7b4cd7cd39874db641fe18
AlbertK10 <103700277+AlbertK10@users.noreply.github.com>
2022-05-11 17:38:36 +0200

    Add files via upload

---
+++ Kalendarz2.Backend/Controllers/EventController.cs
@@ -0,0 +1,26 @@
+﻿using Kalendarz2.Domain.Common;
+using Kalendarz2.Domain.Interfaces.Facades;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Kalendarz2.Backend.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+
+
+    public class EventController : ControllerBase
+    {
+        private readonly IEventFcd _eventFcd;
+
+        public EventController(IEventFcd eventFcd)
+        {
+            _eventFcd = eventFcd;
+        }
+
+        [HttpPost("addevent")]
+        public ActionResult AddEvent([FromBody] AddEventDTO addeventDTO)
+        {
+            return Ok(_eventFcd.AddEvent(addeventDTO));
+        }
+    }
