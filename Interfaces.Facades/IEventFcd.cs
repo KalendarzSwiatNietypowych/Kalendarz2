cd646ad18f9f8dda8de40a6ee9ad179f5b9061f7
AlbertK10 <103700277+AlbertK10@users.noreply.github.com>
2022-05-11 18:06:25 +0200

    Add files via upload

---
+++ Interfaces.Facades/IEventFcd.cs
@@ -0,0 +1,10 @@
+﻿using Kalendarz2.Domain.Common;
+
+namespace Kalendarz2.Domain.Interfaces.Facades;
+
+public interface IEventFcd
+{ 
+    EventDTO AddEvent(AddEventDTO addeventDTO);
+    EventDTO ModifyEvent(ModifyEventDTO modifyeventDTO);
+
+}
