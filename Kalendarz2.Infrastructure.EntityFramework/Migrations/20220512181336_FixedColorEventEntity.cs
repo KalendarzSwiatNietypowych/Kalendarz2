faabca9fdd65c0ba912fd036868eab3eb8253797
Wojciech Łuszczak <wojtekluszczakpl@gmail.com>
2022-05-12 20:14:44 +0200

    Fix color in event entity

---
+++ Kalendarz2.Infrastructure.EntityFramework/Migrations/20220512181336_FixedColorEventEntity.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Kalendarz2.Infrastructure.EntityFramework
+{
+    public partial class FixedColorEventEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ColorB",
+                table: "Events");
+
+            migrationBuilder.DropColumn(
+                name: "ColorG",
+                table: "Events");
+
+            migrationBuilder.RenameColumn(
+                name: "ColorR",
+                table: "Events",
+                newName: "Color");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Color",
+                table: "Events",
+                newName: "ColorR");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ColorB",
+                table: "Events",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ColorG",
+                table: "Events",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
