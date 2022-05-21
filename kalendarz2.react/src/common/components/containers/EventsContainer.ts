8d45c0f7ce2f7a776418f3783004fa5bbcb0254a
Michalele2115 <103456890+Michalmaciej@users.noreply.github.com>
2022-05-21 17:30:47 +0200

    Create component and css event list

--- kalendarz2.react/src/common/components/containers/EventsContainer.ts
+++ kalendarz2.react/src/common/components/containers/EventsContainer.ts
@@ -2,7 +2,40 @@ import styled from "styled-components"
 
 export const EventsContainer = styled.div`
     &&&{
-        background: #3E3E3E;
-        height: 1000%;
+            background: rgb(25,138,103);
+            background: linear-gradient(180deg, rgba(25,138,103,1) 0%, rgba(43,197,152,1) 50%, rgba(25,138,103,1) 100%);
+            border-radius:2rem;
+            width:17%;
+            margin: 1rem 82.3%;
+            position:relative;
+            height:6.0rem;
+        h5{
+            color: white;
+            font-family:'inter';
+            font-size:0.9rem;
+            font-weight:500;
+            text-align:left;
+            margin-right: 3rem;
+            padding:0.9rem 1rem;
+        }
+        h6{
+            color: white;
+            font-size:0.7rem;
+            margin-top: 0.25rem;
+            margin-left: 7%;
+            text-align:left;
+            direction:ltr;
+        }
+        img{
+            width:1.5rem;
+            position:relative;
+            left: 38%;
+            top: 10%;
+            //bottom:5;
+            //right:5;
+            //margin: 1% 5%;
+            background-color:transparent;
+            transform:rotate(-5deg);
+        }
     }
 `
--- kalendarz2.react/src/event/Events.tsx
+++ kalendarz2.react/src/event/Events.tsx
@@ -1,16 +1,28 @@
 import { EventsContainer } from "../common/components/containers/EventsContainer";
+import {EventsContainerMainText} from "../common/components/containers/EventsContainerMainText";
 
 export const Events = () => {
   return (
+    <EventsContainerMainText>
+      <h3>Wydarzenia:</h3>
+      <h4>Piątek 12.06.2022</h4>
     <EventsContainer>
-      <ul>
-        <li>event 1</li>
-        <li>event 2</li>
-        <li>event 3</li>
-        <li>event 4</li>
-        <li>event 5</li>
-        <li>event 6</li>
-      </ul>
+      <h5>Kolokwium zaliczeniowe</h5>
+      <h6>12:00 - 14:00</h6>
+      <h6>Politechnika Rzeszowska</h6>
     </EventsContainer>
+      <h4>Sobota 13.06.2022</h4>
+    <EventsContainer>
+      <h5>Kolokwium zaliczeniowe</h5>
+      <h6>12:00 - 14:00</h6>
+      <h6>Politechnika Rzeszowska</h6>
+    </EventsContainer>
+      <h4>Niedziela 14.06.2022</h4>
+    <EventsContainer>
+      <h5>Kolokwium zaliczeniowe</h5>
+      <h6>12:00 - 14:00</h6>
+      <h6>Politechnika Rzeszowska</h6>
+    </EventsContainer>
+    </EventsContainerMainText>
   );
 };
